b3f2a54d2aa1665db913247ed83eb5459c484eea
kamal <kamalzalagh@gmail.com>
2022-02-11 03:31:38 +0100

    first commit

---
+++ src/main/java/com/example/demo/YourturnFlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourturnFlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourturnFlApplication.class, args);
+	}
+
+}
